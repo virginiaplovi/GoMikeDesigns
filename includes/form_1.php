fba2bee024b6d309466265b2e342f8d05cb178c1
virginiaplovi <virginiaplovi@gmail.com>
2021-04-22 22:05:22 +0100

    ARIA attr added, validator error corrected

--- includes/form_1.php
+++ includes/form_1.php
@@ -9,7 +9,7 @@
 	$input_504 = $_POST['input_504'];
 	$message = $_POST['message'];
 	
-	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+	$to = 'mike@gomikedesigns.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from your website";
